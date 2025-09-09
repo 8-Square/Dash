1f9e02dacf50ac872210859c50f958f618bb1e75
4_Square <104998118+8-Square@users.noreply.github.com>
2025-09-09 13:40:46 +1000

    Initial commit

---
+++ Assets/Scenes/level_one.gd
@@ -0,0 +1,25 @@
+extends Node2D
+
+@onready var player_death = $DeathUI
+@onready var player = $Player
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$DeathUI.visible = false
+	player.connect("died", Callable(self, "on_player_death"))
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if player_death and Input.is_action_just_pressed("Restart"):
+		restart_level()
+
+func on_player_death():
+	player_death.visible = true
+	$AnimationPlayer.play("death")
+	
+func restart_level():
+	$AnimationPlayer.play("restart")
+	player.reset_player()
+	await $AnimationPlayer.animation_finished
+	player_death.visible = false
---
+++ Assets/Scenes/player.gd
@@ -0,0 +1,74 @@
+class_name Player
+extends CharacterBody2D
+
+signal died
+@export var level_start_pos : Node2D
+
+@onready var tilemap = $"../LevelLayout"
+@onready var colorrect = $"../CanvasLayer/ColorRect"
+
+const SPEED = 550
+var can_control = true
+
+func action_name() -> String:
+	if Input.is_action_just_pressed("Up"):
+		return "Up"
+	elif Input.is_action_just_pressed("Down"):
+		return "Down"
+	elif Input.is_action_just_pressed("Left"):
+		return "Left"
+	elif Input.is_action_just_pressed("Right"):
+		return "Right"
+	return ""
+	
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("Restart"):
+		await get_tree().create_timer(0.4).timeout
+		reset_player()
+		return
+	
+	if not can_control:
+		velocity = Vector2.ZERO
+		move_and_slide()
+		return 
+	
+	if velocity == Vector2.ZERO:
+		var action = action_name()
+		match action:
+			"Down":
+				velocity = Vector2.DOWN * SPEED
+			"Up":
+				velocity = Vector2.UP * SPEED
+			"Right":
+				velocity = Vector2.RIGHT * SPEED
+			"Left":
+				velocity = Vector2.LEFT * SPEED
+		
+	move_and_slide()
+	
+	for i in range(get_slide_collision_count()):
+		var collision = get_slide_collision(i)
+		if collision.get_normal().x != 0:
+			velocity.x = 0
+		if collision.get_normal().y != 0:
+			velocity.y = 0
+	check_spike()
+
+
+func check_spike():
+	var cell: Vector2i = tilemap.local_to_map(tilemap.to_local(global_position))
+	var tile_data: TileData = tilemap.get_cell_tile_data(cell)
+	if tile_data:
+		print("Tile Found At", cell, " custom Trap=", tile_data.get_custom_data("Trap"))
+		if tile_data.get_custom_data("Trap") and can_control:
+			can_control = false
+			print("TOUCHING")
+			emit_signal("died")
+
+
+
+func reset_player() -> void:
+	global_position = level_start_pos.global_position
+	velocity = Vector2.ZERO
+	can_control = true
+	print("Player Has Been Reset")
